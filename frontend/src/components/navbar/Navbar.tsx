077eb9ec819e2425485272a248328d590496ff58
chrisfran03 <francischristo03@gmail.com>
2024-08-03 01:46:17 +0530

    remove files from accidental commit

---
+++ frontend/src/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import './navbar.scss'
+const Navbar: React.FC = () => {
+    return (
+        <div>
+            <div className="brand"></div>
+            <div className="hamburger"></div>
+            <div className="menu"></div>
+        </div>
+    )
+}
+
+export default Navbar
