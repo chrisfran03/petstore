0754aa012aafe631acec49e4fe1c4acb01637758
chrisfran03 <francischristo03@gmail.com>
2024-08-03 02:10:53 +0530

    add navbar component

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,11 +1,19 @@
-// import React from 'react'
+import React from 'react'
 import './navbar.scss'
+import { Menu } from "@mui/icons-material"
+import { Link } from "react-router-dom"
 const Navbar: React.FC = () => {
     return (
         <div>
-            <div className="brand"></div>
-            <div className="hamburger"></div>
-            <div className="menu"></div>
+            <div className="brand">Pet Store</div>
+            <div className="hamburger"><Menu /></div>
+            <div className="menu">
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/products">Products</Link></li>
+                    <li><Link to="/products/add">Add Product</Link></li>
+                </ul>
+            </div>
         </div>
     )
 }
