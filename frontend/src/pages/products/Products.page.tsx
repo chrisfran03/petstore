396025a54673d05ebd5b9294ab2f148d5e2652b7
chrisfran03 <francischristo03@gmail.com>
2024-08-03 12:14:05 +0530

    display products in table

--- frontend/src/pages/products/Products.page.tsx
+++ frontend/src/pages/products/Products.page.tsx
@@ -3,6 +3,9 @@ import './products.scss'
 import axios from 'axios';
 import { baseUrl } from '../../constants/url.constant';
 import { IProduct } from '../../types/global.typing';
+import { Button } from "@mui/material";
+import { Edit, Delete } from "@mui/icons-material";
+import moment from "moment";
 
 const Products: React.FC = () => {
     const [products, setProducts] = useState<IProduct[]>([]);
@@ -21,10 +24,43 @@ const Products: React.FC = () => {
         fetchProductsList();
     }, []);
 
-    console.log(products);
+
     return (
         <div className="products">
             <h1>Products List</h1>
+            {
+                products.length === 0 ? (<h1>No Products</h1>) : (
+                    <div className="table-wrapper">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Brand</th>
+                                    <th>Creation Time</th>
+                                    <th>Update Time</th>
+                                    <th>Operations</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    products.map(product => (
+                                        <tr key={product.id}>
+                                            <td>{product.title}</td>
+                                            <td>{product.brand}</td>
+                                            <td>{moment(product.createdAt).fromNow()}</td>
+                                            <td>{moment(product.updatedAt).fromNow()}</td>
+                                            <td>
+                                                <Button variant="outlined" color="warning" sx={{ mx: 3 }}><Edit /></Button>
+                                                <Button variant="outlined" color="error" sx={{ mx: 3 }}><Delete /></Button>
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                )
+            }
         </div>
     );
 };
