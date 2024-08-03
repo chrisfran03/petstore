5a49f8e2577f57d04b607ec031d32ced152f0911
chrisfran03 <francischristo03@gmail.com>
2024-08-03 11:24:45 +0530

    add types

--- frontend/src/pages/products/Products.page.tsx
+++ frontend/src/pages/products/Products.page.tsx
@@ -1,7 +1,9 @@
+import { IProduct } from '../../types/global.typing';
 import './products.scss'
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Products: React.FC = () => {
+    const [products, setProducts] = useState<IProduct[]>([])
     return (
         <div className="products">
             <h1>Products List</h1>
