6f147f00ab5b9cc22faa1a8c7fe5a9bb3c97da87
chrisfran03 <francischristo03@gmail.com>
2024-08-03 09:32:39 +0530

    style homepage and add image

--- frontend/src/pages/home/Home.page.tsx
+++ frontend/src/pages/home/Home.page.tsx
@@ -1,7 +1,17 @@
 import './home.scss'
+import { Button } from "@mui/material"
+import { useNavigate } from 'react-router-dom'
+import dog from "../../assets/images/dog.jpg"
+
 const Home = () => {
+    const redirect = useNavigate();
     return (
-        <div>Home.page</div>
+        <div className="home">
+            <h1>Welcome to Pet Store</h1>
+            <Button variant='outlined' color='primary' onClick={() => redirect("/products")} >Products List</Button>
+            <img src={dog} alt="dog" />
+
+        </div>
     )
 }
 
