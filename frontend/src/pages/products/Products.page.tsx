1260aad43dd4c169d77ba09ac6f354c4e62dfab3
chrisfran03 <francischristo03@gmail.com>
2024-08-03 11:10:27 +0530

    add product list page

---
+++ frontend/src/pages/products/Products.page.tsx
@@ -0,0 +1,12 @@
+import './products.scss'
+import React from 'react';
+
+const Products: React.FC = () => {
+    return (
+        <div className="products">
+            <h1>Products List</h1>
+        </div>
+    );
+};
+
+export default Products
