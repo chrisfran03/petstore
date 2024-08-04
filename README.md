84f1b8be5984acfac829fa2a7eb7fbbdcbaf1624
chrisfran03 <francischristo03@gmail.com>
2024-08-04 17:19:57 +0530

    add readme file

---
+++ README.md
@@ -0,0 +1,9 @@
+This project has React Typescript front end and Asp.NET Core web api backend which communicates with the frontend using HTTP Request. The backend uses an MS SQL Server database through Entity Framework Core ORM.
+To start the project:
+Backend:
+-> cd backend
+-> dotnet run
+
+Frontend:
+-> npm i
+-> npm start
--- backend/Program.cs
+++ backend/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swcxzagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
